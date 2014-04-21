11ddc95369603bc7729a8daf6f9ad808c5ac640b
Ondrej Zdych <zdych.ondrej@gmail.com>
2014-04-21 11:14:52 +0200

    Upd

--- gulpfile.js
+++ gulpfile.js
@@ -4,35 +4,51 @@ var ecstatic = require('ecstatic');
 var traceur = require('gulp-traceur');
 var rjs = require('gulp-requirejs');
 var uglify = require('gulp-uglify');
+var refresh = require('gulp-livereload');
+var clean = require('gulp-clean');
+var lr = require('tiny-lr');
+var lrserver = lr();
 
+var livereloadport = 35729;
 var serverport = 8000;
 
+var srcFolder = 'app';
+var distFolder = 'dist';
+
 gulp.task('scripts', function() {
-  return gulp.src('app/*.js')
-    .pipe(traceur({ sourceMaps: false, modules: 'amd' }))
-    .pipe(gulp.dest('./app-built'));
+  return gulp.src(srcFolder + '/*.js')
+    .pipe(traceur({ modules: 'amd' }))
+
+    .pipe(gulp.dest(distFolder))
+    .pipe(refresh(lrserver));
 });
 
 gulp.task('build', function() {
     rjs({
       baseUrl: '.',
-      out: 'app-built.js',
-      name: 'app/main',
+      out: distFolder + '/all.min.js',
+      name: srcFolder + '/main',
       paths: {
-        app: 'app-built'
+        app: 'dist'
       }
     })
     .pipe(uglify())
-    .pipe(gulp.dest('.'));
+    .pipe(gulp.dest('.'))
+    .pipe(refresh(lrserver));
 });
 
 gulp.task('serve', function() {
   http.createServer(ecstatic({ root: __dirname })).listen(serverport);
+  lrserver.listen(livereloadport);
+});
+
+gulp.task('clean', function() {
+  gulp.src(distFolder, {read: false})
+    .pipe(clean());
 });
 
 gulp.task('watch', function() {
-  gulp.watch('app/*.js', ['scripts']);
+  gulp.watch(srcFolder + '/*.js', ['scripts']);
 });
 
-// Default Task
-gulp.task('default', ['scripts', 'serve', 'watch']);
+gulp.task('default', ['clean', 'scripts', 'serve', 'watch']);
